c4d53ac81d6a8e6db8a1d6f021f9514090c009d7
qinx0 <pandabucchibecker@gmail.com>
2025-02-10 15:12:29 +0100

    Added Files

---
+++ Blocks.py
@@ -0,0 +1,39 @@
+import engine_main # type: ignore
+import engine # type: ignore
+from engine_nodes import Sprite2DNode # type: ignore
+from engine_resources import TextureResource # type: ignore
+from engine_math import Vector2 # type: ignore
+import math
+import engine_draw # type: ignore
+from engine_draw import Color # type: ignore
+
+offRot = [-90,-180,-270,90,180,270]
+
+blocks = TextureResource("Images/blocks.bmp")
+
+# blocks
+class block:
+    def __init__(self, cord, pos, deadly, tag, rot):
+        if not isinstance(cord, Vector2) or not isinstance(pos, Vector2) or not isinstance(tag, str):
+            raise TypeError("'cord' and 'pos' must be of type 'Vector2'")
+        self.cord = cord
+        self.pos = pos
+        self.deadly = deadly
+        self.tag = tag
+        self.rot = math.radians(rot)
+                
+        self.pos.x = self.pos.x * 16# - 8
+        self.pos.y = self.pos.y * 16 + 5
+        
+        self.Block = getBlock(cord)
+        self.Block.position = self.pos
+        self.Block.rotation = self.rot
+        
+        def __repr__(self):
+            return f"block(cord={self.cord}, pos={self.pos})"
+        
+def getBlock(cord):
+    b = Sprite2DNode(texture=blocks, frame_count_x=6, frame_count_y=6, transparent_color = Color(1,0,1), playing=False)
+    b.frame_current_x = int(cord.x)
+    b.frame_current_y = int(cord.y)
+    return b
